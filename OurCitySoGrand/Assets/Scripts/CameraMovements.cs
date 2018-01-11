760a65d6b0f0c6dc47b334a963f977a6965b84d1
Goaph <marko.beocanin2@gmail.com>
2018-01-11 22:27:19 +1100

    Added Player Movements and Basic Mouse-Look

---
+++ OurCitySoGrand/Assets/Scripts/CameraMovements.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovements : MonoBehaviour {
+
+    public float minY = 0.4f;
+    public float maxY = -0.7f;
+    public float cameraVerticalSpeed = 2.0f;
+
+    private Vector3 euler;
+
+    // Update is called once per frame
+    void Update () {
+        //Rotating the Camera (not the player) based off mouse pos on the X axis
+        float v = -cameraVerticalSpeed * Input.GetAxis("Mouse Y"); //is negative so that it's not inverse
+        transform.Rotate(v, 0, 0);
+
+        
+        Debug.Log(transform.rotation.x);
+        /*if (transform.rotation.x <= maxY)
+        {
+            Debug.Log("UpperY reached!");
+            transform.rotation.x = maxY;
+        }
+
+        if (transform.rotation.x >= minY)
+        {
+            Debug.Log("LowerY reached!");
+            transform.rotation.x = minY;
+        }
+        */
+    }
+}
---
+++ OurCitySoGrand/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Transform player;
+   
+    public float forwardSpeed = 5.0f;
+    public float sideSpeed = 5.0f;
+    public float cameraHorizontalSpeed = 30.0f;
+    
+
+    void Update () {
+        //Moves Character Forward and Backwards, Left and Right
+        //Gets the Up and Down Keys, then left and right
+        float moveForward = Input.GetAxis("Vertical") * forwardSpeed;
+        float moveSide = Input.GetAxis("Horizontal") * sideSpeed;
+
+        //Multiplied by DeltaTime to be consistent across all devices
+        moveForward *= Time.deltaTime;
+        moveSide *= Time.deltaTime;
+
+        //Player's position is actually translated
+        player.Translate(0, 0, moveForward);
+        player.Translate(moveSide, 0, 0);
+
+        //Mouse-Look 
+        //Rotating the Character based off mouse position on the Y axis
+        float h = cameraHorizontalSpeed * Input.GetAxis("Mouse X");
+        player.Rotate(0, h, 0);
+
+        
+
+    
+    }
+}
