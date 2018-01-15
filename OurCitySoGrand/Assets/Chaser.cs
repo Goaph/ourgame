ef4a6daebf106b7a3ea9835c326417e55d981264
Goaph <marko.beocanin2@gmail.com>
2018-01-15 17:20:40 +1100

    Added Basic Chasing AI

---
+++ OurCitySoGrand/Assets/Chaser.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chaser : MonoBehaviour {
+
+    public Transform chaser;
+    public Transform player;
+
+    public float speed = 10f;
+    public float stopDistance = 3f;
+
+	void Update () {
+
+        chaser.LookAt(player);
+        if (Vector3.Distance(chaser.position, player.position) > stopDistance)
+        {
+            
+            chaser.Translate(0, 0, speed * Time.deltaTime);
+        }else
+        {
+            Debug.Log("Gotcha!");
+        }
+        
+
+	}
+}
